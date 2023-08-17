6360ecbb2782263f3ca87a8c1184355f2f7c6e86
Wincracker11 <142412102+Wincracker11@users.noreply.github.com>
2023-08-17 10:51:39 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,37 +1,42 @@
-import json, time, requests
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 from flask import Flask, render_template, request
+import json, time, requests
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        q = request.form.get('question')
-
+        q = request.form.get("question")
         body = json.dumps(
             {
                 "version" : "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
-                "input" : {
-                    "prompt" : q
-                }
+                "input" : {"prompt":q}
             }
         )
-        
         headers = {
-            'Authorization': 'Token r8_WfZBCnoUdklV0uHHULKjqIZmMfSjWrx08gYy5',
-            'Content-Type': 'application/json'
+            "Authorization" : "Token r8_75O6bDt9CtXQ4sUgkXVI7DKUkx8OX211UN2Vc",
+            "Content-Type" : "application/json"
         }
-
-        output = requests.post('https://api.replicate.com/v1/predictions',data=body,headers=headers)
+        output = requests.post("https://api.replicate.com/v1/predictions", data=body,headers=headers)
         time.sleep(10)
-        get_url = output.json()['urls']['get']
-        get_result = requests.post(get_url,headers=headers).json()['output']
-
-
-        return(render_template('index.html', result=get_result[0]))
+        get_url = output.json()["urls"]["get"]
+        get_result = requests.post(get_url, headers=headers).json()["output"]
+        return(render_template("index.html", result=get_result[0]))
     else:
-        return(render_template('index.html', result='waiting'))
+        return(render_template("index.html", result="waiting"))
     
 if __name__ == "__main__":
     app.run()
+
+
+# In[ ]:
+
+
+
+
